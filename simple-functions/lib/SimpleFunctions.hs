9f96933f87ef185f7f2bccd4e3849e606d7fa344
SevanBadal <sgolnaza@gmail.com>
2023-01-28 13:30:22 -0500

    test cases added for each question in the simple-functions project

--- simple-functions/lib/SimpleFunctions.hs
+++ simple-functions/lib/SimpleFunctions.hs
@@ -7,3 +7,26 @@ module SimpleFunctions where
 
 exampleSum :: Int -> Int -> Int
 exampleSum x y = x + y
+
+-- 1. Define a function called basicMath that takes a pair of Integrals and returns a tuple with the sum, difference, product and quotient.
+-- For example basicMath 8 2 should return (10, 6, 16, 4). 
+-- Be careful with the division one!
+-- test command: cabal run basic-math-test -v0
+
+-- 2. Define a function factors that returns a list of all the factors of the number given.
+-- For example, factors 12 returns [1,2,3,4,6,12].
+-- Hint: this is do-able reasonably if you use a list comprehension.
+-- test command: cabal run factors-test -v0
+
+-- 3. Write a function compute that takes a tuple with an operator (given as a single character)
+-- and two numbers and returns the value from computing the result of that operation on the given numbers.
+-- For example, compute ('+',3,4) should return 7.
+-- test command: cabal run compute-test -v0
+
+-- 4. Write a function corn that accepts the number of ears of corn the customer is purchasing and outputs the total price.
+-- The corn is priced according to the following four statements:
+-- If the customer is purchasing less than a dozen ears, the price per ear is $0.50.
+-- If the customer is purchasing 12 to 23 ears, the price per ear is $0.45.
+-- If the customer is purchasing 24 to 35 ears, the price per ear is $0.40.
+-- If the customer is purchasing more than 35 ears, the price per ear is $0.35.
+-- test command: cabal run corn-test -v0
---
+++ simple-functions/tests/BasicMathTest.hs
@@ -0,0 +1,21 @@
+module Main where
+import SimpleFunctions (basicMath)
+import Test.HUnit
+import qualified System.Exit as Exit
+
+integralTuple :: Integral a => (a,a,a,a) -> (a,a,a,a)
+integralTuple x = x
+
+basicMathTestOne :: Test
+basicMathTestOne = TestCase (assertEqual "should return (10,6,16,4)" (integralTuple (10, 6, 16, 4)) (basicMath 8 2))
+
+basicMathTestTwo :: Test
+basicMathTestTwo = TestCase (assertEqual "should return (9,-1,20,0)" (integralTuple (9, (-1), 20, 0)) (basicMath 4 5))
+
+tests :: Test
+tests = TestList [TestLabel "BASIC MATH TEST 1" basicMathTestOne, TestLabel "BASIC MATH TEST 2" basicMathTestTwo]
+
+main :: IO ()
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
