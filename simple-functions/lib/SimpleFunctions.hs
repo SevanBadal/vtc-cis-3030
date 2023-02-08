a3c6d15baa736a7181c597f43ef39a8ef320fd70
Andrew C <andrewclark3942@gmail.com>
2023-02-08 01:03:05 -0500

    Simple Functions assignment - All cabal test cases passed

--- simple-functions/lib/SimpleFunctions.hs
+++ simple-functions/lib/SimpleFunctions.hs
@@ -10,15 +10,28 @@ exampleSum x y = x + y
 -- Be careful with the division one!
 -- test command: cabal run basic-math-test -v0
 
+basicMath :: (Integral a) => a -> a -> (a, a, a, a)
+basicMath x y = (x + y, x - y, x * y, div x y)
+
 -- 2. Define a function named factors that returns a list of all the factors of the number given.
 -- For example, factors 12 returns [1,2,3,4,6,12].
 -- Hint: this is do-able reasonably if you use a list comprehension.
 -- test command: cabal run factors-test -v0
 
+factors :: (Integral a) => a -> [a]
+factors number = [x | x <- [1..number], (number `mod` x) == 0]
+
 -- 3. Write a function compute that takes a tuple with an operator (given as a single character)
 -- and two numbers and returns the value from computing the result of that operation on the given numbers.
 -- For example, compute ('+',3,4) should return 7.
 -- test command: cabal run compute-test -v0
+
+compute :: (Fractional a) => (Char, a, a) -> a
+compute (operator, a, b) 
+    | operator == '+' = a + b
+    | operator == '-' = a - b
+    | operator == '*' = a * b
+    | operator == '/' = a / b
 
 -- 4. Write a function corn that accepts the number of ears of corn the customer is purchasing and outputs the total price.
 -- The corn is priced according to the following four statements:
@@ -27,3 +40,10 @@ exampleSum x y = x + y
 -- If the customer is purchasing 24 to 35 ears, the price per ear is $0.40.
 -- If the customer is purchasing more than 35 ears, the price per ear is $0.35.
 -- test command: cabal run corn-test -v0
+
+corn :: (Fractional x, Ord x) => x -> x
+corn number
+    | number < 12 = number * 0.5
+    | number < 24 = number * 0.45
+    | number < 36 = number * 0.4
+    | otherwise = number * 0.35
